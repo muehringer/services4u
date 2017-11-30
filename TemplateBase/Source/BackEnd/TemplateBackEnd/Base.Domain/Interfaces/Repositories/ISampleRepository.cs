989cc8483fcba74c0186a831d4327c04f3b6e49f
Alexandre Muehringer Alves <alexandre.alves@giespp.com.br>
2017-11-30 17:05:36 -0200

    Arquitetura

---
+++ TemplateBase/Source/BackEnd/TemplateBackEnd/Base.Domain/Interfaces/Repositories/ISampleRepository.cs
@@ -0,0 +1,15 @@
+﻿using Base.Domain.Entities;
+using Base.Domain.Interfaces.UnitOfWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Base.Domain.Interfaces.Repositories
+{
+    public interface ISampleRepository : IUnitOfWork<Sample, int>
+    {
+    }
+}
