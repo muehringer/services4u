65407388435b29d6fb0b9bc1a39b2dfcefc48512
Alexandre Muehringer Alves <alexandre.alves@giespp.com.br>
2017-12-04 15:04:40 -0200

    Arquitetura BackEnd-FrontEnd

--- TemplateBase/Source/FrontEnd/templatefrontend/src/components/Body.js
+++ TemplateBase/Source/FrontEnd/templatefrontend/src/components/Body.js
@@ -1,16 +1,22 @@
-﻿import React, { Component } from 'react';
+﻿import React, {Component} from 'react';
 import $ from 'jquery';
 
 class Body extends Component {
 
-	//constructor(props) {
-    	//	super(props);
-    		//this.state = {user: '', password: ''};
-	//}
-
-	login() {
-
+	constructor(props) {
+    		super(props);
+    		this.state = {user: '', password: ''};
 		debugger;
+		//usar no botão ou aqui no construtor
+		this.login = this.login.bind(this); 
+		this.loginChange = this.loginChange.bind(this);
+	}
+
+	loginChange(event) {
+		this.setState({user: event.target.form.user.value, password: event.target.form.password.value});
+	}
+
+	login(event) {
 
 		var token = null;
 		var token_type = null;
@@ -18,7 +24,8 @@ class Body extends Component {
 		$.ajax({
 			  method: "POST",
 			  url: "http://localhost:12004/token",
-			  data: { username: "Fulano", password: "1234", grant_type: "password" }, 
+			  //data: { username: "Fulano", password: "1234", grant_type: "password" }, 
+			  data: { username: this.state.user, password: this.state.password, grant_type: "password" }, 
 			  headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
 			  contentType: "application/json; charset=utf-8",
 			  dataType: 'json',
@@ -29,13 +36,15 @@ class Body extends Component {
 			  success: function (response) {
 			  debugger;
 
-				token: response.access_token;
-				token_type: response.token_type;
+				token = response.access_token;
+				token_type = response.token_type;
 			  },
 			  error: function (e) {
                 
 			  }
 		});
+
+		event.preventDefault();
 	}
 
 	render() {
@@ -45,15 +54,15 @@ class Body extends Component {
 				<div className="row">
 					<form className="col s12">
 						<div className="input-field col s6">
-							  <input placeholder="User" id="user" type="text" className="validate" value='' />
+							  <input placeholder="User" id="user" type="text" className="validate" value={this.state.user} onChange={this.loginChange} />
 							  <label htmlFor="user">User</label>
 						</div>
 						<div className="input-field col s6">
-							  <input placeholder="Password" id="password" type="password" className="validate" value='' />
+							  <input placeholder="Password" id="password" type="password" className="validate" value={this.state.password} onChange={this.loginChange} />
 							  <label htmlFor="password">Password</label>
 						</div>
 
-						<button className="btn waves-effect waves-light" onClick={this.login()} type="button" name="action">Login
+						<button className="btn waves-effect waves-light" onClick={this.login} type="button" name="action">Login
 							<i className="material-icons right">send</i>
 						</button>
 					</form>
