ce85a69a939522a0bce0d9fe8db19ecd38022de5
Alexandre Muehringer Alves <alexandre.alves@giespp.com.br>
2018-01-30 19:20:57 -0200

    Arquitetura BackEnd-FrontEnd

---
+++ TemplateBase/Source/BackEnd/TemplateBackEnd/Base.Infrastructure/Logs/Log.cs
@@ -0,0 +1,61 @@
+﻿using Serilog;
+using Serilog.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Base.Infrastructure.Logs
+{
+    public static class Log
+    {
+        private static Logger _log = null;
+
+        public static void Register()
+        {
+            if (_log == null) {
+                _log = new LoggerConfiguration()
+                    .ReadFrom.AppSettings("log")
+                    .CreateLogger();
+
+                _log.Debug("Log Criado");
+            }
+        }
+
+        public static void Information(string message)
+        {
+            _log.Information(message);
+        }
+
+        public static void Information(string message, params object[] values)
+        {
+            _log.Information(message, values);
+        }
+
+        public static void Debug(string message)
+        {
+            _log.Debug(message);
+        }
+
+        public static void Debug(string message, params object[] values)
+        {
+            _log.Debug(message, values);
+        }
+
+        public static void Error(Exception ex, string message = "")
+        {
+            _log.Error(ex, message);
+        }
+
+        public static void Error(Exception ex, string message = "", params object[] values)
+        {
+            _log.Error(ex, message, values);
+        }
+
+        public static void Fatal(string message)
+        {
+            _log.Fatal("", message);
+        }
+    }
+}
---
+++ TemplateBase/Source/BackEnd/TemplateBackEnd/Base.Infrastructure/Logs/LogAudit.cs
@@ -0,0 +1,36 @@
+﻿using Serilog;
+using Serilog.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Base.Infrastructure.Logs
+{
+    public static class LogAudit
+    {
+        private static Logger _log = null;
+
+        public static void Register()
+        {
+            if (_log == null) {
+                _log = new LoggerConfiguration()
+                    .ReadFrom.AppSettings("audit")
+                    .CreateLogger();
+
+                _log.Debug("Log Criado");
+            }
+        }
+
+        public static void Information(string message)
+        {
+            _log.Information(message);
+        }
+
+        public static void Information(string message, params object[] values)
+        {
+            _log.Information(message, values);
+        }
+    }
+}
